89c01dd9a8c68f77046209274141ef12b59ec9f3
Akash-Chaudhari <akashchaudhari0002@gmail.com>
2025-09-19 00:07:37 +0530

    Servlet JSP demo project

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to Spring MVC with Tomcat 10.1.6!");
+        return "home";
+    }
+
+    @GetMapping("/first")
+    public String first(Model model) {
+        model.addAttribute("message", "This is the FIRST controller mapping!");
+        return "home";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<html>
+<head>
+    <title>Spring MVC Home</title>
+</head>
+<body>
+<h2>${message}</h2>
+</body>
+</html>
